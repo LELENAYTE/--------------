ae428824010ccf90cc4db2fa725978c3652d9e4e
LELENAYTE <gretis.wes@gmail.com>
2021-10-18 22:50:35 +0300

    Готова

--- dz22.py
+++ dz22.py
@@ -1,4 +1,4 @@
-vvod = input("введите строку")
+vvod = input("Введите строку: ")
 #print(vvod)
 k = int(input("Введите число k: "))
 schetchik = 0
@@ -6,4 +6,5 @@ for i in vvod:
     if i.isdigit():
         schetchik += 1
         if schetchik == k:
-            print (i)
+            print (str(k) + "-я цифра в строке:\t" + str(i))
+            break
