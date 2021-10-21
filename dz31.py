733d4cb8cfd1599d060f20d0f3acce421400327c
LELENAYTE <gretis.wes@gmail.com>
2021-10-21 20:28:26 +0300

    .

---
+++ dz31.py
@@ -0,0 +1,9 @@
+def saymont():
+    b = list()#создали список b
+    x = input()#вводим элементы списка
+    while x != "":#проверка на пустую строку
+        b.append(x)
+        x = input()
+    return(b)
+
+print(saymont())
