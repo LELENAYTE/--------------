15510ebeccc591c342eac4358a5ef69ad7e5f052
LELENAYTE <90473634+LELENAYTE@users.noreply.github.com>
2021-10-30 09:02:32 +0300

    Update dz32.py

--- dz32.py
+++ dz32.py
@@ -1,11 +1,10 @@
 def season(num):
-
     if num == 12 or 1 <= num <= 2:
         sezon = "Зима"
     elif 3 <= num <= 5:
         sezon = "Весна"
     elif 6 <= num <= 8:
-        sezon: str = "Лето"
+        sezon = "Лето"
     elif 9 <= num <= 11:
         sezon = "Осень"
     else:
