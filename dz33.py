c72d9bc5ae0050d79ba24a947b2ac02e789ab2b6
LELENAYTE <90473634+LELENAYTE@users.noreply.github.com>
2021-11-22 23:38:51 +0300

    Update dz33.py
    Забыл залить исправленный

--- dz33.py
+++ dz33.py
@@ -2,14 +2,12 @@ def fib(n):
     pred_value = 0
     value = 1
     if n < 1:
-        print("Не надо так делать")
         return 0
-    if n < 1:
-        return 0
-    for _ in range(n):
+    for i in range(n):
         hold = value
         value += pred_value
         pred_value = hold
+    return value
 
 
 n = int(input("введите n:\t"))
