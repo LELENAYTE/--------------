a7fbfcbe3b24177550e45e8d150febd35cec3642
LELENAYTE <90473634+LELENAYTE@users.noreply.github.com>
2021-11-27 00:56:06 +0300

    Update dz35.py

--- dz35.py
+++ dz35.py
@@ -2,9 +2,7 @@ from dz31 import vvodspiska
 
 
 def sred_arifm(b):
-    resultat = sum(b) / len(b)
-    return resultat
-
+    return sum(b) / len(b)
 
 b = [float(item) for item in vvodspiska()]
 print(sred_arifm(b))
